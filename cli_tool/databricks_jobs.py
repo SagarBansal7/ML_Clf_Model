628ecc657ab724d0c9c40ed35f7a9445e2fd9072
sagarbansal719@gmail.com <sagarbansal719@gmail.com>
2025-03-25 01:28:39 +0000

    Testing Workflow

--- cli_tool/databricks_jobs.py
+++ cli_tool/databricks_jobs.py
@@ -8,6 +8,8 @@ class DatabricksJobManager:
     def __init__(self, databricks_host, databricks_token, catalog, schema):
         self.databricks_host = databricks_host
         self.databricks_token = databricks_token
+        self.catalog = catalog
+        self.schema = schema
         self.params = { 
             "catalog": catalog,
             "schema": schema
@@ -40,7 +42,7 @@ class DatabricksJobManager:
                     "task_key": job_name.replace(" ", "_").lower(),
                     "spark_python_task": {
                          "python_file": notebook_path,
-                         "base_parameters": self.params
+                         "base_parameters": [self.catalog, self.schema]
                     },
                     "environment_key": "db_job_key"
                 }
